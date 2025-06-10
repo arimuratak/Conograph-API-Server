26983abdbf8b630576042e6c231d6f7e10d0adee
arimuratak <149344913+arimuratak@users.noreply.github.com>
2025-06-10 10:04:23 +0900

    Add files via upload

--- api_server/Conograph_API.py
+++ api_server/Conograph_API.py
@@ -1,4 +1,5 @@
 # api_server.py
+import shutil
 from flask import Flask, request, send_file, jsonify
 import subprocess
 import os
@@ -14,32 +15,53 @@ app = Flask(__name__)
 
 @app.route('/parse_cntl', methods = ['POST'])
 def parse_cntl_file():
-    assert 'file' in request.files
+    reset_workspace (work)
+    #assert 'file' in request.files
     file = request.files['file']
-    assert isinstance (file.name, str)
-    #assert file.name == 'cntl.imp.xml',  f"ファイル名が不正です: {file.filename}"
+    #assert isinstance (file.name, str)
     path = os.path.join (work, file.name)
     file.save (path)
-    assert os.path.exists (path)
+    #assert os.path.exists (path)
     param_file, hist_file, _ = read_cntl_inp_xml (path)
     ans = [param_file, hist_file]
     return jsonify ({'required_files' : ans})
 
+def reset_workspace (workSpace):
+    for fname in os.listdir (workSpace):
+        if 'PeakSearch.exe' in fname: continue
+
+        path = os.path.join (workSpace, fname)
+        
+        if os.path.isdir (path):
+            shutil.rmtree (path)
+        else:
+            os.remove (path)
+
 @app.route("/run_cpp", methods = ["POST"])
 def run_cpp_with_cntl():
     #os.makedirs("work", exist_ok=True)
-
+    #paths = []
     for key in request.files:
         f = request.files[key]
         path = os.path.join(work, f.filename)
+        #paths.append (path)
         f.save(path)
+
+    cntlPath = os.path.join (work, 'cntl.inp.xml')
+    _,_,out_path = read_cntl_inp_xml (cntlPath)
+    out_path = os.path.join (work, out_path)
+    out_folder = os.path.dirname (out_path)
+    if out_folder is not None:
+        os.makedirs (out_folder, exist_ok=True)
+
+    #paramPath, histPath = paths
     os.chdir (work)
     result = subprocess.run(['./PeakSearch'],
                     capture_output=True, text=True)
+    #result = subprocess.run('PeakSearch.exe')
     os.chdir (current_dir)
-    cntlPath = os.path.join (work, 'cntl.imp.xml')
-    _,_,out_path = read_cntl_inp_xml (cntlPath)
-    out_path = os.path.join (work, out_path)
+    
+
     if os.path.exists (out_path):
         return send_file(out_path, as_attachment = True), 200
     else:
