2995d05827694136b1ad0f79a072c5216c6716ea
arimuratak <149344913+arimuratak@users.noreply.github.com>
2025-06-10 13:13:23 +0900

    Add files via upload

--- api_server/Conograph_API.py
+++ api_server/Conograph_API.py
@@ -5,66 +5,54 @@ import subprocess
 import os
 from dataIO import read_cntl_inp_xml
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-work = os.path.join (current_dir, 'work')
-exePath = os.path.join (work, 'PeakSearch')
-assert os.path.exists (work)
-assert os.path.exists (exePath)
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_cntl = os.path.join (CURRENT_DIR, 'cntl.inp.xml')
+PATH_param, PATH_hist, PATH_out =  read_cntl_inp_xml (PATH_cntl)
+FOLDER_out = os.path.dirname (PATH_out)
+if FOLDER_out is not None: os.makedirs (FOLDER_out, exist_ok = True)
+#work = os.path.join (current_dir, 'work')
+PATH_exe = os.path.join (CURRENT_DIR, 'PeakSearch')
+#assert os.path.exists (work)
+#assert os.path.exists (exePath)
 
 app = Flask(__name__)
 
-@app.route('/parse_cntl', methods = ['POST'])
-def parse_cntl_file():
-    reset_workspace (work)
-    #assert 'file' in request.files
-    file = request.files['file']
-    #assert isinstance (file.name, str)
-    path = os.path.join (work, file.name)
-    file.save (path)
+#@app.route('/parse_cntl', methods = ['POST'])
+#def parse_cntl_file():
+#    #assert 'file' in request.files
+#    file = request.files['file']
+#    #assert isinstance (file.name, str)
+#    path = os.path.join (work, file.name)
+#    file.save (path)
     #assert os.path.exists (path)
-    param_file, hist_file, _ = read_cntl_inp_xml (path)
-    ans = [param_file, hist_file]
-    return jsonify ({'required_files' : ans})
-
-def reset_workspace (workSpace):
-    for fname in os.listdir (workSpace):
-        if 'PeakSearch' in fname: continue
-        #if 'PeakSearch.exe' in fname: continue
-
-        path = os.path.join (workSpace, fname)
-        
-        if os.path.isdir (path):
-            shutil.rmtree (path)
-        else:
-            os.remove (path)
+#    param_file, hist_file, _ = read_cntl_inp_xml (path)
+#    ans = [param_file, hist_file]
+#    return jsonify ({'required_files' : ans})
 
 @app.route("/run_cpp", methods = ["POST"])
 def run_cpp_with_cntl():
     #os.makedirs("work", exist_ok=True)
     #paths = []
+    pathDict = {'xml' : PATH_param, 'dat' : PATH_hist,
+                'histogramIgor' : PATH_hist}
     for key in request.files:
         f = request.files[key]
-        path = os.path.join(work, f.filename)
+        fname = f.name
+        suffix = fname.split('.')[-1]
+        path = pathDict[suffix]
+        path = os.path.join (CURRENT_DIR, path)
         #paths.append (path)
         f.save(path)
 
-    cntlPath = os.path.join (work, 'cntl.inp.xml')
-    _,_,out_path = read_cntl_inp_xml (cntlPath)
-    out_path = os.path.join (work, out_path)
-    out_folder = os.path.dirname (out_path)
-    if out_folder is not None:
-        os.makedirs (out_folder, exist_ok=True)
-
     #paramPath, histPath = paths
-    os.chdir (work)
-    result = subprocess.run([exePath],
+    
+    result = subprocess.run([PATH_exe],
                     capture_output=True, text=True)
     #result = subprocess.run('PeakSearch.exe')
-    os.chdir (current_dir)
     
 
-    if os.path.exists (out_path):
-        return send_file(out_path, as_attachment = True), 200
+    if os.path.exists (PATH_out):
+        return send_file(PATH_out, as_attachment = True), 200
     else:
         return jsonify({"error": "出力ファイルがありません"}), 500
 
