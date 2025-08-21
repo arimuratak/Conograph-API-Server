904d5748b996e0008de2c811fc5fbadeb7e73add
arimuratak <149344913+arimuratak@users.noreply.github.com>
2025-08-21 13:14:36 +0900

    Add files via upload

--- api_server/Conograph_API.py
+++ api_server/Conograph_API.py
@@ -2,7 +2,24 @@ from flask import Flask, request, send_file, jsonify
 import subprocess
 import os
 import stat
-from dataIO import read_cntl_inp_xml
+import xml.etree.ElementTree as ET
+
+def read_cntl_inp_xml (path):
+    # XMLファイルを読み込む
+    tree = ET.parse(path)  # ファイル名を適宜変更
+    root = tree.getroot()
+
+    # 各要素の取得
+    control_param = root.find('.//ControlParamFile')
+    control_param_file = control_param.text.strip() if control_param is not None else None
+
+    histogram_file = root.find('.//HistogramDataFile/FileName')
+    histogram_file_name = histogram_file.text.strip() if histogram_file is not None else None
+
+    outfile = root.find('.//Outfile')
+    outfile_name = outfile.text.strip() if outfile is not None else None
+    return control_param_file, histogram_file_name, outfile_name
+
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 os.chdir(CURRENT_DIR)
@@ -12,6 +29,13 @@ FOLDER_out = os.path.dirname (PATH_out)
 if FOLDER_out is not None: os.makedirs (FOLDER_out, exist_ok = True)
 PATH_exe = os.path.join (CURRENT_DIR, 'PeakSearch')
 PATH_log = os.path.join (CURRENT_DIR, 'LOG_PEAKSEARCH.txt')
+
+if os.name == 'nt':
+    PATH_exe = 'PeakSearch.exe'
+else:
+    PATH_exe = os.path.join (CURRENT_DIR, 'PeakSearch')
+    if not os.access (PATH_exe, os.X_OK):
+            os.chmod(PATH_exe, os.stat (PATH_exe).st_mode | stat.S_IEXEC)
 
 app = Flask(__name__)
 
@@ -23,20 +47,16 @@ def run_cpp_with_cntl():
     if os.path.exists (PATH_log): os.remove (PATH_log)
 
     pathDict = {'xml' : PATH_param, 'dat' : PATH_hist,
-                'histogramIgor' : PATH_hist}
+                'histogramigor' : PATH_hist}
     for key in request.files:
         f = request.files[key]
         fname = f.name
-        suffix = fname.split('.')[-1]
+        suffix = fname.split('.')[-1].lower()
         path = pathDict[suffix]
         path = os.path.join (CURRENT_DIR, path)
         f.save(path)
     
-    if not os.access (PATH_exe, os.X_OK):
-       os.chmod(PATH_exe, os.stat (PATH_exe).st_mode | stat.S_IEXEC)
-    result = subprocess.run([PATH_exe],
-                    capture_output=True, text=True)
-    #result = subprocess.run('PeakSearch.exe')
+    result = subprocess.run([PATH_exe])
     
     if os.path.exists (PATH_out):
         return send_file(PATH_out, as_attachment = True), 200
